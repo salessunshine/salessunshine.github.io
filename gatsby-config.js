42dfc395f716a2604113c25ba3c6ec195a4bb7c0
Sheryl Garniel <sales.sunshine@marsu.edu.ph>
2025-09-05 21:15:03 +0800

    Changed name

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://monumental-chimera-b1ace3.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Sunshine Sales',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Sunshine Sales | Software Engineer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Cute langs`,
     // Optional: Twitter account handle
     author: `@rfitzio`,
     // Optional: Github account URL
